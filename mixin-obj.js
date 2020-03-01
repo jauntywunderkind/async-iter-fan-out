e68464bbcde7375790902f622b0efe575e08e595
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2020-03-01 14:56:18 -0500

    stub out some mixins to implement

---
+++ mixin-obj.js
@@ -0,0 +1,5 @@
+/** mixin event reader to this object */
+export function mixinEventReader( obj){
+	throw new Error("Not implemented")
+}
+export default mixinEventReader
---
+++ mixin-type.js
@@ -0,0 +1,5 @@
+/** create a new class derived from `klass` that mixes in `EventReader` */
+export function mixinEventReaderType( klass){
+	throw new Error("Not implemented")
+}
+export default mixinEventReader
