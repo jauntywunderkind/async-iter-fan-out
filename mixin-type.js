7d4757f31b4b8a73e9f9342c32c2fc50564833a2
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2020-03-01 15:01:48 -0500

    mixin-type hack impl

--- mixin-type.js
+++ mixin-type.js
@@ -1,5 +1,16 @@
 /** create a new class derived from `klass` that mixes in `EventReader` */
 export function mixinEventReaderType( klass){
+	const mixed= ({[ klass.name]: class extends klass{
+		constructor( ...arg){
+			if( this._onArg){
+				const [superArg, readerArg] = this._onArg.call( this, ...arg)
+			}
+			constructor.super_.call( this, ...(superArg|| []))
+			EventReader.call( this, ..(readerArg|| []))
+		}
+	}})[ klass.name]
+	const props= Object.getOwnPropertyDescriptors( EventReader.prototype)
+	Object.defineProperties( mixed.prototype, props)
 	throw new Error("Not implemented")
 }
 export default mixinEventReader
