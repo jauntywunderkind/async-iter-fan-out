93eaed1454f9ab5052106acec499e06eeffdae33
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2020-02-29 20:02:31 -0500

    add base test for not-ok emitter less reader

--- .test/index.js
+++ .test/index.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
+import tape from "tape"
 import EventReader from "../event-reader.js"
+import { EventEmitter} from "events"
+
+tape( "no listener without an emitter", function( t){
+	t.plan( 1)
+	const reader= new EventReader()
+	try{
+		reader.listener("nope")
+		t.fail("got a listener")
+	}catch(ex){
+		t.pass("properly failed to get a listener")
+	}
+})
